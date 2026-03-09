5ffd38ab98550bd0bd3d17dc3e622e8248e9a858
Tom Antjon <tom.antjon@hogent.be>
2026-03-09 11:24:46 +0100

    first commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,22 @@
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model/json/JSONModel"], (Controller, MessageToast, JSONModel) => {
+  "use strict";
+
+  return Controller.extend("be.hogent.hellofiorivc.controller.App", {
+    onInit() {
+      console.log("Controller Loaded");
+
+      let obj = {
+        buttonText: "this is dynamic text",
+      };
+
+      let customModel = new JSONModel(obj);
+      this.getView().setModel(customModel, "custom");
+    },
+    onPress: function (evt) {
+      MessageToast.show(evt.getSource().getId() + " Pressed");
+      let cModel = this.getView().getModel("custom");
+      let cObj = cModel.getData();
+      console.log(cObj);
+    },
+  });
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["be/hogent/hellofiorivc/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
